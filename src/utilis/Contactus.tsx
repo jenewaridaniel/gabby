9c87768642082a646cbb7ea7014afe7c22881969
Daniel <jenwaridaniel123@gmail.com>
2025-05-16 13:15:38 +0100

    added firebase to contact us

--- src/utilis/Contactus.tsx
+++ src/utilis/Contactus.tsx
@@ -260,7 +260,7 @@ const ContactUs: React.FC = () => {
             </svg>
           </div>
           <h3 className="mt-3 text-base font-medium text-gray-900">Address</h3>
-          <p className="mt-2 text-base text-gray-500">123 Sunshine Blvd, Port Harcourt City</p>
+          <p className="mt-2 text-base text-gray-500">Plot 11 Primegate Avenue off G.U Ake Road, Eliogbolo, Port Harcourt, Rivers State, Nigeria</p>
         </div>
       </div>
     </div>
