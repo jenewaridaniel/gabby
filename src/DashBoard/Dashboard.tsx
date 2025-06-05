2d79f242e1e7a87d885c39d10cac3969cae0ca71
Daniel <jenwaridaniel123@gmail.com>
2025-06-05 10:52:37 +0100

    mehn coding is getting a bit interesting of late

--- src/DashBoard/Dashboard.tsx
+++ src/DashBoard/Dashboard.tsx
@@ -237,7 +237,7 @@ const Dashboard = () => {
           </button>
         </nav>
         <div className="p-4">
-          <button className="flex items-center w-full px-4 py-3 rounded-lg text-gray-600 hover:bg-gray-100">
+          <button onClick={LogOut} className="flex items-center w-full px-4 py-3 rounded-lg text-gray-600 hover:bg-gray-100">
             <FiLogOut className="w-5 h-5 mr-3" />
             Logout
           </button>
